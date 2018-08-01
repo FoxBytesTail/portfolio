1b746978577a703d28d4242bbdd2c6f3a5bb8b4c
youngmin lee <youngmin@youngminlee.net>
2018-07-31 18:05:46 -0700

    initial commit

---
+++ src/main.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import ElementUI  from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import App from './App.vue';
+
+Vue.use(ElementUI)
+
+Vue.component('Element', Element)
+Vue.config.productionTip = false
+
+new Vue({
+  el: '#app',
+  render: h => h(App)
+}).$mount('#app')
+
+
